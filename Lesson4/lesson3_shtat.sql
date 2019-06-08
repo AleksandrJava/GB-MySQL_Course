f191b7a317c4ada900c8b1cef3dc76fc8f9d894a
AleksandrJava <44460921+AleksandrJava@users.noreply.github.com>
2019-06-08 19:50:19 +0300

    Add files via upload

---
+++ Lesson4/lesson3_shtat.sql
@@ -0,0 +1,57 @@
+-- MySQL dump 10.13  Distrib 8.0.13, for Linux (x86_64)
+--
+-- Host: localhost    Database: lesson3
+-- ------------------------------------------------------
+-- Server version	8.0.13
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `shtat`
+--
+
+DROP TABLE IF EXISTS `shtat`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `shtat` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(25) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `lastname` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `depart` int(11) DEFAULT NULL,
+  `position` varchar(40) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `salary` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `dept_sotr` (`depart`),
+  CONSTRAINT `dept_sotr` FOREIGN KEY (`depart`) REFERENCES `depart` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `shtat`
+--
+
+LOCK TABLES `shtat` WRITE;
+/*!40000 ALTER TABLE `shtat` DISABLE KEYS */;
+INSERT INTO `shtat` VALUES (1,'Сергей','Григорьев',3,NULL,25000),(2,'Алена','Митрохина',1,NULL,35000),(3,'Георгий','Мухин',2,NULL,45000),(5,'Артём','Веснушкин',6,NULL,50000),(6,'Анатолий','Башлыков',1,'Бухгалтер',30000),(7,'Евгений','Полянский',4,'Кадровик',50000),(10,'Владимир','Рустамов',5,NULL,40000);
+/*!40000 ALTER TABLE `shtat` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-06-08 19:48:01
